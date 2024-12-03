bd49be9f3a13dc9e317741bfb5d4416a5854d349
Dennis <sfnandy65066506@gmail.com>
2024-12-03 15:53:48 +0800

    把更新的日期年拿掉，只保留月和日

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,11 +9,10 @@ export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
     const d = new Date(date);
     if (isNaN(d.getTime())) return '';
     
-    const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const day = String(d.getDate()).padStart(2, '0');
     
-    return `${year}/${month}/${day}`;
+    return `${month}/${day}`;
   };
 
   return (
